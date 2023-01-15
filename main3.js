5eec9d8329f2739c004a1682d1ee5497c252d3aa
Supermacaco86 <99079977+Supermacaco86@users.noreply.github.com>
2023-01-15 16:30:35 -0300

    Funcion learningPath

--- main3.js
+++ main3.js
@@ -1,9 +1,41 @@
 
-// Getters y Setters:
+
+
+
+function isArray(subjet) {
+    return Array.isArray(subjet);
+};
 
 
 function requireParams(param){
     throw new Error(param + " este parametro es obligatorio");
+}
+
+function createLearningPath({
+    name = requireParams("name"),
+    courses = [],
+}){
+    const private = {
+        "_name": name,
+        "_courses": courses,
+    };
+    const public = {
+        get name(){ 
+            return private["_name"];
+           },
+        set name(newName){
+            if(newName.length != 0){
+                private["_name"] = newName;
+            }else{
+                console.warn("Tu nombre debe tener al menos un caracter")
+            }
+           },
+
+        get courses(){ 
+            return private["_courses"];
+           },
+    };
+    return public
 }
 
 function createStudent({
@@ -19,27 +51,46 @@ function createStudent({
          // objeto vacío.
   const private = { // encapsulamiento: Esto no se va a poder editar. Para lograr 
     "_name": name, // esto estamos utilizando los scopes, osea esta variable al
-  };             // sacarla des scope la convertimos en inaccesible. 
+    "_learningPaths": learningPaths,  // sacarla des scope la convertimos en inaccesible.
+};             
   const public = { // A partir de acá si podemo editar.
     age: age,
         email: email,
         approvedCourses: approvedCourses,
-        learningPaths: learningPaths,
         socialMedia: {
             twitwer: twitwer,
             instagram: instagram,
             faceboock: faceboock,
-        },
-       get name(){
-        return private["_name"];
-       },
-       set name(newName){
-        if(newName.length != 0){
-            private["_name"] = newName;
-        }else{
+        }, // Getters y Setters:
+       
+        get name(){ 
+             return private["_name"];
+             },
+        set name(newName){
+            if(newName.length != 0){
+                private["_name"] = newName;
+             }else{
             console.warn("Tu nombre debe tener al menos un caracter")
-        }
-       },
+            }
+        },
+      
+        get learningPaths(){ 
+            return private["_learningPaths"];
+           },
+        set learningPaths(newLearningPaths){
+            if(!newLearningPaths.name){
+                console.warn("Tu learningPath, no tiene la propiedad nombre");
+                return
+            }else if(!newLearningPaths.courses){
+                console.warn("Tu learningPath, no tiene courses");
+                return
+            }else if(!isArray(newLearningPaths.courses)){
+                console.warn("Tu learningPath, no es una lista de courses");
+                return
+            }else{
+                private["_learningPaths"].push(newLearningPaths);
+            }
+           }, 
   }; 
     return public
 }
@@ -50,3 +101,5 @@ const pedro = createStudent({
      email: "Pedro@pedro.com",
      twitwer: "pedro123",
 });
+
+pedro.learningPaths = "Nueva ruta de aprendizajes";
